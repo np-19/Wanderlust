f1e86638ba29c34f42bd2cb12752b6f2e7a01260
Shiv 50477 <ak6270293@gmail.com>
2026-05-01 15:41:06 +0530

    abc

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV != "production") {
   require("dotenv").config();
 }
-
+//express app
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
